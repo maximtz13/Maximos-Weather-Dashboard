55112b918e2a29b668bfe23acbdcbbed830d09c2
Maximoo <maximtz99@gmail.com>
2022-08-24 11:55:13 -0500

    updated script.js/ working on setting up code for displaying city weather for day

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,3 +25,63 @@ class CityWeather {
     }
 };
 
+class CurrentWeather extends CityWeather {
+    windSpeed = 0;
+    uv = {
+        index: 0,
+        color: '',
+    };
+    constructor(temp, humidity, windSpeed) {
+        super(temp, humidity);
+        this.windSpeed = windSpeed;
+    }
+
+    setUV(uvIndex) {
+        this.uv.index = uvIndex;
+        this.uv.color = this.generateUVIndexColor(uvIndex);
+        return this;
+    }
+};
+
+class WeatherData {
+    city = {
+        cityName: '',
+        lat: '',
+        lon: '',
+    }
+
+    currentDay;
+
+    constructor(cityName, lat, lon) {
+        this.city.cityName = cityName;
+        this.city.lat = lat;
+        this.city.lon = lon;
+    }
+
+    setCurrentDay(currentDay) {
+        this.currentDay = currentDay;
+    }
+};
+
+function submitSearch(event) {
+    event.preventDefault();
+
+    const userInput = getUserInput();
+    
+    if (userInput) {
+        startWeatherData(userInput);
+    } else {
+        document.getElementById('city-name').value = '';
+    }
+};
+
+function getUserInput() {
+    let userInput = document.getElementById('city-name').value;
+    userInput = userInput.trim();
+    return userInput;
+}
+
+const API_KEY = '4e700b938e7abb20c0a4650d77ff125e';
+
+
+document.getElementById('search-city').addEventListener('submit', submitSearch);
